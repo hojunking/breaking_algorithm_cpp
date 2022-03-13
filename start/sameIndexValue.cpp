cf9f0d273a04336d8734d38c54ab481fb65c851f
song <hojunking@gmail.com>
2022-03-13 18:51:23 +0900

    update 0313

---
+++ start/sameIndexValue.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void binarySearch(int n, vector<int>& v){
+    int cnt =0, mid=0,low=0,high=n;
+    //cout << high << " " << low << "\n";
+    while(low <= high){
+        cnt++;
+        //cout << "cycle = " << cnt <<"\n";
+        mid = (low + high) /2;
+        if (v[mid] == mid){
+            cout << mid << "\n" << cnt <<"\n";
+            return;
+        }
+        else if(v[mid] > mid){
+            high = mid -1;
+        }
+        else
+            low = mid +1;
+    }
+    cout << -1 << "\n" << cnt << "\n";
+    return;
+}
+
+int main(void){
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for(int i =0; i <n; i++){
+        cin >> v[i];
+    }
+    binarySearch(n,v);
+    return 0;
+}
--- start/twoMatrixDouble.cpp
+++ start/twoMatrixDouble.cpp
@@ -4,35 +4,39 @@
 using namespace std;
 typedef vector<vector<int>> matrix_t;
 
-void mult(int n, matrix_t A, matrix_t B,matrix_t& C ){
+void mult(int n,int k,int m, matrix_t A, matrix_t B,matrix_t& C )
+{
    for(int i =1; i<=n; i++){
-       for(int j =1; j<=n ; j++){
-           C[i][j] += A[i][j] * B[i][j];
+       for(int j =1; j<=m ; j++){
+           C[i][j] =0;
+           for(int u =1; u <= k; u++)
+            C[i][j] += A[i][u] * B[u][j];
        }
    }
 }
 
-int main(void){
-    int n;
-    cin >> n;
+int main(void)
+{
+    int n,k,m;
+    cin >> n >> k >> m;
 
     matrix_t A(n+1, vector<int>(n+1));
-    matrix_t B(n+1, vector<int>(n+1));
-    matrix_t C(n+1, vector<int>(n+1));
+    matrix_t B(k+1, vector<int>(k+1));
+    matrix_t C(m+1, vector<int>(m+1));
    
     for (int i = 1; i <=n;i++){
-        for(int j=1; j<=n; j++)
+        for(int j=1; j<=k; j++)
             cin >> A[i][j];
     }
-    for (int i = 1; i <=n;i++){
-        for(int j=1; j<=n; j++)
+    for (int i = 1; i <=k;i++){
+        for(int j=1; j<=m; j++)
             cin >> B[i][j];
     }
-    
+    mult(n,k,m,A,B,C);
     for (int i = 1; i <=n;i++){
-        for(int j=1; j<=n; j++)
-            cout << C[i][j] << " ";
-        cout << "\n";
+        for(int j=1; j<=m; j++)
+            cout << C[i][j] <<" ";
+        cout <<"\n";
     }
     
 }
