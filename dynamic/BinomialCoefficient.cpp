fbdecfe8cfa515fc2732934c78e15c7c08b562b2
song <hojunking@gmail.com>
2022-04-01 19:01:04 +0900

    update 0401

--- dynamic/BinomialCoefficient.cpp
+++ dynamic/BinomialCoefficient.cpp
@@ -6,20 +6,69 @@ using namespace std;
 typedef unsigned long long LongInteger;
 
 int Max;
+
+
 LongInteger bin2(int n, int k){ //tabulization
-   vector<LongInteger>;
-    
+    if(k > n /2)
+        k = n -k;
+        
+    vector<vector<LongInteger>> B(n + 1, vector<LongInteger>(n + 1));
     for (int i = 0; i <= n; i++)
         for (int j = 0; j <= min(i, k); j++)
             if (j == 0 || j == i)
                 B[i][j] = 1;
             else
                 B[i][j] = (B[i - 1][j] + B[i - 1][j - 1]) % Max;
+   
+    for(int i = 0; i<= n; i++){
+        for(int j =0; j<=min(i, k) ;j++){
+            cout << B[i][j] << " ";
+        }
+    cout << "\n";
+    }
+
     return B[n][k];
 }
+
+
+LongInteger bin3(int n, int k){
+    if(k > n /2)
+        k = n -k;
+    vector<LongInteger> B(k+1,0);
+    B[0]=1;
+
+    for(int i =1; i < n+1; i++){
+        int j = min(i,k);
+        while(j > 1){
+            B[j] = (B[j] + B[j -1]) % Max;
+            cout << B[j] <<" ";
+            j--;
+        }
+        B[j] = i;
+        cout << "\n";
+    }
+    return B[k];
+}
+
+LongInteger bin4(int n, int k){
+    
+    vector<LongInteger> B(n+1,0);
+    
+    for(int i =0; i<=n; i++){
+        for(int j = i; j >=0; j--){
+            if(j ==0 || j ==i)
+                B[j] = 1;
+            else
+                B[j] = (B[j] + B[j-1]) % Max;
+        }
+    }
+    return B[k];
+}
+
 
 int main(void){
     int n, k;
     cin >> n >> k >> Max;
-    cout << bin2(n, k) <<"\n";
+    cout << bin3(n, k) <<"\n";
+    //cout << bin2(n, k) <<"\n";
 }
---
+++ dynamic/BinomialCoefficient2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define min(x, y) (x) < (y) ? (x) : (y)
+
+typedef unsigned long long int LongInteger;
+
+int Max;
+
+LongInteger bin3(int n, int k) {
+    if (k > n / 2)
+        k = n - k;
+    LongInteger* B = (LongInteger*)malloc((k+1) * sizeof(LongInteger));
+    memset(B,0,k*sizeof(LongInteger));
+
+    B[0] = 1;
+
+    for (int i = 1; i < n + 1; i++) {
+        int j = min(i, k);
+        while (j > 0) {
+            B[j] = (B[j] + B[j - 1]) % Max;
+            j--;
+        }
+    }
+    LongInteger result = B[k];
+    free(B);
+    return result;
+}
+
+
+int main(void) {
+    int n, k;
+
+    scanf("%d %d %d",&n,&k,&Max);
+    printf("%llu\n",bin3(n,k));
+}
