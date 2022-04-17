bfc0042e5489012de31152642fc78edbefe6a0d0
song <hojunking@gmail.com>
2022-04-18 01:57:29 +0900

    ddibalEgg update!

---
+++ divide&conquer/ddibalEgg.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num;
+    int cnt=1;
+    int sum=0;
+    cin >> num;
+    while(sum<num)
+    {
+    sum+=cnt++;
+    }
+    cout << cnt-1;
+    return 0;
+}
